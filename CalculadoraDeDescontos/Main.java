01778d2b4b46220daf5e8240bc52ce4d8a52668a
Ezequielzz <ezequielsamueldl@gmail.com>
2023-02-13 15:17:10 -0300

    primeira parte da aula fo fia 13/02/23

---
+++ CalculadoraDeDescontos/Main.java
@@ -0,0 +1,14 @@
+package CalculadoraDeDescontos;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        CalcularDescontos cDescontos = new CalcularDescontos();
+        cDescontos.calcula20();
+        cDescontos.calcula30();
+
+    }
+}
