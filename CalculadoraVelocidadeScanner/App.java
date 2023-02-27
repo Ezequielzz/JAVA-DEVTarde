0082545c1772c00c72fcbd58f3c7d70ac9dde168
Ezequielzz <ezequielsamueldl@gmail.com>
2023-02-27 14:40:09 -0300

    POO CaljculadoraFrequencia

---
+++ CalculadoraVelocidadeScanner/App.java
@@ -0,0 +1,9 @@
+package CalculadoraVelocidadeScanner;
+
+public class App {
+    public static void main(String[] args) {
+        
+        CalculadoraScanner cScanner = new CalculadoraScanner();
+        cScanner.calcular();
+    }
+}
---
+++ CalculadoraVelocidadeScanner/CalculadoraScanner.java
@@ -0,0 +1,21 @@
+package CalculadoraVelocidadeScanner;
+
+import java.util.Scanner;
+
+public class CalculadoraScanner {
+    public void calcular() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe a Distância Percorrida");
+        double distanciaPercorrida = sc.nextDouble();
+        System.out.println("A distância percorrida foi de " + distanciaPercorrida + " km");
+        System.out.println("Informe o Tempo Gasto na Viagem");
+        double tempoGasto = sc.nextDouble();
+        System.out.println("Otempo Gasto na Viagem foi de"
+                + tempoGasto + " horas");
+        sc.close();
+        double VelocidadeMedia = distanciaPercorrida / tempoGasto;
+        System.out.println("A Velocidade Media foi de "
+                + VelocidadeMedia + " km/h");
+    }
+
+}
