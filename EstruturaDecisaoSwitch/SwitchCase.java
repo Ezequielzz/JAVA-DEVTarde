5214cc51733aa1d4ff4c8c7f839dfa2ee574c81d
Ezequielzz <ezequielsamueldl@gmail.com>
2023-03-20 15:56:04 -0300

    20/03/2023

--- EstruturaDecisaoSwitch/SwitchCase.java
+++ EstruturaDecisaoSwitch/SwitchCase.java
@@ -32,26 +32,47 @@ public class SwitchCase {
 
     public void mes() {
         System.out.println("Digite o número do mês");
-        String mes = sc.nextInt();
+        String mes = sc.next();
         switch (mes) {
-            case "":
-                System.out.println();
+            case "1":
+                System.out.println(mes + "é Janeiro");
                 break;
-            case "":
-                System.out.println();
+            case "2":
+                System.out.println(mes + "é Fevereiro");
                 break;
-            case "":
-                System.out.println();
+            case "3":
+                System.out.println(mes + "é Março");
                 break;
-            case "":
-                System.out.println();
+            case "4":
+                System.out.println(mes + "é Abril");
                 break;
-            case "":
-                System.out.println();
+            case "5":
+                System.out.println(mes + "é Maio");
+            case "6":
+                System.out.println(mes + "é Junho");
+                break;
+            case "7":
+                System.out.println(mes + "é Julho");
+                break;
+            case "8":
+                System.out.println(mes + "é Agosto");
+                break;
+            case "9":
+                System.out.println(mes + "é Setembro");
+                break;
+            case "10":
+                System.out.println(mes + "é Outubro");
+                break;
+            case "11":
+                System.out.println(mes + "é Novembro");
+                break;
+            case "12":
+                System.out.println(mes + "é Dezembro");
 
             default:
-                System.out.println();
                 break;
         }
+
+}
 
 }
