7ca2178aa5e9db19c269066bdf64a74607580b67
Ezequielzz <ezequielsamueldl@gmail.com>
2023-05-16 10:45:23 -0300

    a

--- IteracaoFor/ExercicioFor.java
+++ IteracaoFor/ExercicioFor.java
@@ -1,9 +1,11 @@
 package IteracaoFor;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class ExercicioFor {
     Scanner sc = new Scanner(System.in);
+    Random rd = new Random();
 
     public void exercicio1() {
         int vetor[] = new int[5];
@@ -115,16 +117,47 @@ public class ExercicioFor {
         int vetorPar[] = new int[contPar];
         int vetorImpar[] = new int[contImpar];
         // distribuindo os valores nos vetores
-        contPar=0;
-        contImpar=0;
+        contPar = 0;
+        contImpar = 0;
         for (int i = 0; i < vetorNumeros.length; i++) {
-            if (vetorNumeros[i]%2==0) {
-                vetorPar[contPar]=vetorNumeros[i];
+            if (vetorNumeros[i] % 2 == 0) {
+                vetorPar[contPar] = vetorNumeros[i];
                 contPar++;
             } else {
-                vetorImpar[contImpar]=vetorNumeros[i];
+                vetorImpar[contImpar] = vetorNumeros[i];
                 contImpar++;
             }
         }
+    }
+
+    public void extra1() {
+        int[][] vetor = new int[4][4];
+
+        // preenche a matriz com o resultado da multiplicação
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                vetor[i][j] = (i + 1) * (j + 1);
+            }
+        }
+
+        // imprime a matriz na tela
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                vetor[i][j] = Random.nextInt(0, 10);
+            }
+            System.out.println();
+        }
+    }
+    public void extra2() {
+        int[][] vetor = new int[4][4];
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.println(vetor[i][j] + " ");
+            System.out.println();
+            }
+        }
+
+
     }
 }
