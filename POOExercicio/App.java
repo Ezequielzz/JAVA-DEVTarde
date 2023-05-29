5131efbc8504983fc60bffa62641cc3d27502430
Ezequielzz <ezequielsamueldl@gmail.com>
2023-05-29 17:01:46 -0300

    poo

--- POOExercicio/App.java
+++ POOExercicio/App.java
@@ -2,7 +2,7 @@ package POOExercicio;
 
 public class App {
     public static void main(String[] args) {
-        Pessoas obj1 = new Pessoas();
+        Pessoas obj1 = new Pessoas(null, 0, 0, 0, 0, 0)
         obj1.imprimirDados();
     }
 }
