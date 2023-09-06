582ca33acd2b93cac9b7d467efea3876cef8cf97
Ezequielzz <Ezequielzz>
2023-09-06 15:58:39 -0300

    Subindo

--- SA3GUILayout/src/Calculadora2.java
+++ SA3GUILayout/src/Calculadora2.java
@@ -1,9 +1,19 @@
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.*;
+import javax.swing.*;
 
 public class Calculadora2 extends JPanel {
     public Calculadora2() {
         super();
         this.add(new JLabel("Calculadora2"));
+
+        JPanel painelPrincipal = new JPanel(new BorderLayout());
+        this.add(painelPrincipal);
+
+        JPanel painelCentral = new JPanel(new GridLayout(2,0));
+        this.add(painelPrincipal);
+        painelPrincipal.add(BorderLayout.CENTER);
+
+        painelCentral.add(new JLabel("Insira sua Altura"));
+        painelCentral.add(new JTextField(10));
     }
 }
