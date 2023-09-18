94bc9a5216089ab75e3b087e5dbfed793d638a03
Ezequielzz <Ezequielzz>
2023-09-18 16:56:21 -0300

    GUI Cards&Tabbed

--- GUIExercicioExtras/src/Exercicio1.java
+++ GUIExercicioExtras/src/Exercicio1.java
@@ -1,15 +1,10 @@
-import java.util.logging.Handler;
+import java.awt.FlowLayout;
 
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
-import java.awt.*;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 
 public class Exercicio1 extends JFrame {
     private String painel1 = "Card 1";
@@ -19,10 +14,12 @@ public class Exercicio1 extends JFrame {
         // Construtor
         super("Exercicio 1 Extra");
         JPanel painelPrincipal = new JPanel();
+        painelPrincipal.setLayout(new FlowLayout());
         String botao[] = {painel1, painel2};
         JButton botaoBack = new JButton(botao[0]);
         JButton botaoNext = new JButton(botao[1]);
 
+        painelPrincipal.add(botaoBack);
         painelPrincipal.add(botaoNext);
 
         // Criando os Cards
