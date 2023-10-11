679dd2245327719f0400ed7ff060fc5e6eaa9746
Ezequielzz <ezequielsamueldl@gmail.com>
2023-10-11 16:59:05 -0300

    Subindo

--- SA4TodoListApp/src/TodoList.java
+++ SA4TodoListApp/src/TodoList.java
@@ -1,11 +1,17 @@
-import java.util.*;
+import java.util.ArrayList;
 import java.util.List;
-
 import javax.swing.*;
-import java.awt.*;
 
-public class TodoList {
-    // atributos
+import java.awt.*;
+import java.awt.event.*;
+
+public class TodoList extends JFrame {
+
+    ImageIcon imagem = new ImageIcon(getClass().getResource("resources/iconLixeira.png"));
+    public JLabel lIconLixeira = new JLabel(imagem);
+
+
+    // Atributos
     private JPanel mainPanel;
     private JTextField taskInputField;
     private JButton addButton;
@@ -15,117 +21,194 @@ public class TodoList {
     private JButton markDoneButton;
     private JComboBox<String> filterComboBox;
     private JButton clearCompletedButton;
+
     private List<Task> tasks;
 
-    // construtor
-public TodoList() {
-    // Configuração da janela principal
-    super("To-Do List App");
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setSize(400, 300);
-    // Inicializa o painel principal
-    mainPanel = new JPanel();
-    mainPanel.setLayout(new BorderLayout());
-    // Inicializa a lista de tasks e a lista de tasks concluídas
-    tasks = new ArrayList<>();
-    listModel = new DefaultListModel<>();
-    taskList = new JList<>(listModel);
-    // Inicializa campos de entrada, botões e JComboBox
-taskInputField = new JTextField();
-addButton = new JButton("Adicionar");
-deleteButton = new JButton("Excluir");
-markDoneButton = new JButton("Concluir");
-filterComboBox = new JComboBox<>(new String[]{"Todas", "Ativas",
-"Concluídas"});
-clearCompletedButton = new JButton("Limpar Concluídas");
-// Configuração do painel de entrada
-JPanel inputPanel = new JPanel(new BorderLayout());
-inputPanel.add(taskInputField, BorderLayout.CENTER);
-inputPanel.add(addButton, BorderLayout.EAST);
-// Configuração do painel de botões
-JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-buttonPanel.add(deleteButton);
-buttonPanel.add(markDoneButton);
-buttonPanel.add(filterComboBox);
-buttonPanel.add(clearCompletedButton);
-// Adiciona os componentes ao painel principal
-mainPanel.add(inputPanel, BorderLayout.NORTH);
-mainPanel.add(new JScrollPane(taskList), BorderLayout.CENTER);
-mainPanel.add(buttonPanel, BorderLayout.SOUTH);
-// Adiciona o painel principal à janela
+    public TodoList() {
 
-this.add(mainPanel);
+        // Configuração da janela principal
+        super("To-Do List App");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(470, 300);
+        // Inicializa o painel principal
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        // Inicializa a lista de tasks e a lista de tasks concluídas
+        tasks = new ArrayList<>();
+        listModel = new DefaultListModel<>();
+        taskList = new JList<>(listModel);
 
-// Configuração de Listener para os Eventos
+        // Inicializa campos de entrada, botões e JComboBox
+        taskInputField = new JTextField();
+        addButton = new JButton("Adicionar");
+
+        // Adicionar tarefa
+        addButton.addActionListener(e -> {
+            addTask();
+        });
+
+        taskInputField.addKeyListener(new KeyListener() {
+            public void keyTyped(KeyEvent e) {
+                if (e.getKeyChar() == KeyEvent.VK_ENTER) {
+                    addTask();
+                }
+            }
+        
+            public void keyPressed(KeyEvent e) {
+                // Implemente o que você deseja fazer quando uma tecla é pressionada.
+            }
+        
+            public void keyReleased(KeyEvent e) {
+                // Implemente o que você deseja fazer quando uma tecla é liberada.
+            }
+        });
+
+       
+
+        deleteButton = new JButton("Excluir");
+        markDoneButton = new JButton("Concluir");
+        filterComboBox = new JComboBox<>(new String[] { "Todas", "Ativas",
+                "Concluídas" });
+        clearCompletedButton = new JButton("Limpar Concluídas");
+        // Configuração do painel de entrada
+        JPanel inputPanel = new JPanel(new BorderLayout());
+        inputPanel.add(taskInputField, BorderLayout.CENTER);
+        inputPanel.add(addButton, BorderLayout.EAST);
+        // Configuração do painel de botões
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        buttonPanel.add(deleteButton);
+        buttonPanel.add(markDoneButton);
+        buttonPanel.add(filterComboBox);
+        buttonPanel.add(clearCompletedButton);
+        buttonPanel.add(lIconLixeira);
+        // Adiciona os componentes ao painel principal
+        mainPanel.add(inputPanel, BorderLayout.NORTH);
+        mainPanel.add(new JScrollPane(taskList), BorderLayout.CENTER);
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+        // Adiciona o painel principal à janela
+        this.add(mainPanel);
+        // Configuração de Listener para os Eventos
+
+        // Crie uma instância do seu Handler
+        Handler handler = new Handler();
 
 
-private void addTask() {
-    // Adiciona uma nova task à lista de tasks
-    String taskDescription = taskInputField.getText().trim();// remove espaços vazios
-    if (!taskDescription.isEmpty()) {
-        Task newTask = new Task(taskDescription);
-        tasks.add(newTask);
-        updateTaskList();
-        taskInputField.setText("");
+        // Adicione o Handler como ouvinte de ação para o botão
+        deleteButton.addMouseListener(handler);
+
     }
-}
 
-private void deleteTask() {
-    // Exclui a task selecionada da lista de tasks
-    int selectedIndex = taskList.getSelectedIndex();
-    if (selectedIndex >= 0 && selectedIndex < tasks.size()) {
-        tasks.remove(selectedIndex);
-        updateTaskList();
-    }
-}
+    // Criação de Listas e eventos
 
-private void markTaskDone() {
-    // Marca a task selecionada como concluída
-    int selectedIndex = taskList.getSelectedIndex();
-    if (selectedIndex >= 0 && selectedIndex < tasks.size()) {
-        Task task = tasks.get(selectedIndex);
-        task.setDone(true);
-        updateTaskList();
-    }
-}
-
-private void filterTasks() {
-    // Filtra as tasks com base na seleção do JComboBox
-    String filter = (String) filterComboBox.getSelectedItem();
-    listModel.clear();
-    for (Task task : tasks) {
-        if (filter.equals("Todas") || (filter.equals("Ativas") &&
-                !task.isDone()) || (filter.equals("Concluídas") && task.isDone())) {
-            listModel.addElement(task.getDescription());
+    // Criação do método de adicionar lista.
+    private void addTask() {
+        // Adiciona uma nova task à lista de tasks
+        String taskDescription = taskInputField.getText().trim();// remove espaços vazios
+        if (!taskDescription.isEmpty()) {
+            Task newTask = new Task(taskDescription);
+            tasks.add(newTask);
+            updateTaskList();
+            taskInputField.setText("");
         }
     }
-}
 
-private void clearCompletedTasks() {
-    // Limpa todas as tasks concluídas da lista
-    List<Task> completedTasks = new ArrayList<>();
-    for (Task task : tasks) {
-        if (task.isDone()) {
-            completedTasks.add(task);
+    private void deleteTask() {
+        // Exclui a task selecionada da lista de tasks
+        int selectedIndex = taskList.getSelectedIndex();
+        if (selectedIndex >= 0 && selectedIndex < tasks.size()) {
+            tasks.remove(selectedIndex);
+            updateTaskList();
         }
     }
-    tasks.removeAll(completedTasks);
-    updateTaskList();
-}
 
-private void updateTaskList() {
-    // Atualiza a lista de tasks exibida na GUI
-    listModel.clear();
-    for (Task task : tasks) {
-        listModel.addElement(task.getDescription() + (task.isDone() ?
-
-                " (Concluída)" : ""));
+    private void markTaskDone() {
+        // Marca a task selecionada como concluída
+        int selectedIndex = taskList.getSelectedIndex();
+        if (selectedIndex >= 0 && selectedIndex < tasks.size()) {
+            Task task = tasks.get(selectedIndex);
+            task.setDone(true);
+            updateTaskList();
+        }
     }
-}
 
-public void run() {
-    // Exibe a janela
-    this.setVisible(true);
-}
-}
+    private void filterTasks() {
+        // Filtra as tasks com base na seleção do JComboBox
+        String filter = (String) filterComboBox.getSelectedItem();
+        listModel.clear();
+        for (Task task : tasks) {
+            if (filter.equals("Todas") || (filter.equals("Ativas") &&
+                    !task.isDone()) || (filter.equals("Concluídas") && task.isDone())) {
+                listModel.addElement(task.getDescription());
+            }
+        }
+    }
+
+    private void clearCompletedTasks() {
+        // Limpa todas as tasks concluídas da lista
+        List<Task> completedTasks = new ArrayList<>();
+        for (Task task : tasks) {
+            if (task.isDone()) {
+                completedTasks.add(task);
+            }
+        }
+        tasks.removeAll(completedTasks);
+        updateTaskList();
+    }
+
+    private void updateTaskList() {
+        // Atualiza a lista de tasks exibida na GUI
+        listModel.clear();
+        for (Task task : tasks) {
+            listModel.addElement(task.getDescription() + (task.isDone() ? " (Concluída)" : ""));
+        }
+    }
+
+    
+
+    public class Handler implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            // TODO Auto-generated method stub
+           
+            if( JOptionPane.showConfirmDialog(null, "Deseja Excluir Essa Tarefa?",
+            "Excluindo Tarefa...", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+                deleteTask();
+            }
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            // TODO Auto-generated method stub
+
+        }
+
+    }
+
+    
+
+    // Método
+    public void run() {
+        // Exibe a janela
+        this.setVisible(true);
+    }
 }
