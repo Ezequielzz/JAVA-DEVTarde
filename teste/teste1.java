c1bca5b5c6d39e1f2666490b2bbba1da85d1a01c
Ezequielzz <Ezequielzz>
2023-10-25 16:26:52 -0300

    Subindo pasta de teste

---
+++ teste/teste1.java
@@ -0,0 +1,18 @@
+package teste;
+
+public class teste1 {
+    public static void main(String[] args) {
+
+        int loopVal = 0;
+    
+        while(loopVal<5) {
+            System.out.println("Teste");
+            loopVal++;
+    
+        }
+        
+    }
+        
+    
+
+}
