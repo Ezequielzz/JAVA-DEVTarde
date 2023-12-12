d20258468f3931401d901c30d6aa894adabb9afe
Ezequielzz <123770595+Ezequielzz@users.noreply.github.com>
2023-12-12 16:45:57 -0300

    Update ClientesControl.java

--- ProjetoFinal2Semestre/src/main/java/Controller/ClientesControl.java
+++ ProjetoFinal2Semestre/src/main/java/Controller/ClientesControl.java
@@ -2,6 +2,7 @@ package main.java.Controller;
 
 import javax.swing.JOptionPane;
 import main.java.Connection.ClientesDAO;
+import main.java.Model.Clientes;
 
 public class ClientesControl {
 
@@ -16,16 +17,29 @@ public class ClientesControl {
         return true;
     }
 
+    // Método para verificar se o cliente já está cadastrado no banco de dados
+    public boolean clienteCadastrado(String cpfCliente) {
+        ClientesDAO clientesDAO = new ClientesDAO();
+        for (Clientes cliente : clientesDAO.listarTodos()) {
+            if (cliente.getCpfCliente().equals(cpfCliente)) {
+                return true; // O cliente já está cadastrado
+            }
+        }
+        return false; // O cliente não está cadastrado
+    }
+
     // Método para cadastrar um novo cliente no banco de dados
     public void cadastrar(String nomeCliente, String cpfCliente) {
 
         try {
             if (validarNome(nomeCliente)) {
-                new ClientesDAO().cadastrar(nomeCliente, cpfCliente); // Realiza o cadastro no banco de dados
-                JOptionPane.showMessageDialog(null, "Novo Cliente Cadastrado!");
-
+                if (!clienteCadastrado(cpfCliente)) {
+                    new ClientesDAO().cadastrar(nomeCliente, cpfCliente); // Realiza o cadastro no banco de dados
+                    JOptionPane.showMessageDialog(null, "Novo Cliente Cadastrado!");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Cliente Já Cadastrado!", "Erro!", JOptionPane.ERROR_MESSAGE);
+                }
             }
-
         } catch (IllegalArgumentException e) {
             JOptionPane.showMessageDialog(null, "Nome Inválido!", "Erro!", JOptionPane.ERROR_MESSAGE);
         }
